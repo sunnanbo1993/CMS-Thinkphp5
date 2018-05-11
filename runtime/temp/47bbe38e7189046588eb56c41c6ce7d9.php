04646a9830a50a7aa48c8e788bf57f93211eb3b7
sunnanbo <838617101@qq.com>
2018-05-11 15:53:45 +0800

    第一次提交未提交全面，补充提交

---
+++ runtime/temp/47bbe38e7189046588eb56c41c6ce7d9.php
@@ -0,0 +1,110 @@
+<?php if (!defined('THINK_PATH')) exit(); /*a:5:{s:77:"D:\data\wwwroot\yitian\admin\public/../application/index\view\index\menu.html";i:1525853238;s:77:"D:\data\wwwroot\yitian\admin\public/../application/index\view\index\resp.html";i:1525851212;s:79:"D:\data\wwwroot\yitian\admin\public/../application/index\view\index\topnav.html";i:1525851207;s:80:"D:\data\wwwroot\yitian\admin\public/../application/index\view\index\welcome.html";i:1525845424;s:77:"D:\data\wwwroot\yitian\admin\public/../application/index\view\index\list.html";i:1525855410;}*/ ?>
+<!-- Main content wrapper -->
+<!-- Note -->
+
+
+<!-- Widgets -->
+<div class="widgets">
+    <form action="" class="form-list">
+        <div>
+            <label class="control-label">品牌名称:</label>
+            <select name="" id="">
+                <option value="">请选择</option>
+            </select>
+        </div>
+        <div>
+            <label class="control-label">关键字:</label>
+            <input type="text" name="search" placeholder="搜索..." id="ac" />
+        </div>
+        <div>
+            <input type="submit" value="搜索" />
+        </div>
+    </form>
+	<div class="widget">
+        <table cellpadding="0" cellspacing="0" width="100%" class="sTable">
+            <thead>
+                <tr>
+                    <td>编号</td>
+                    <td>品牌</td>
+                    <td>型号</td>
+                    <td>官方售价（元）</td>
+                    <td>操作</td>
+                </tr>
+            </thead>
+            <tbody align="center">
+                <tr>
+                    <td><a href="#" title="" class="webStatsLink">980</a></td>
+                    <td>OPPO</td>
+                    <td>MP551538</td>
+                    <td><span class="statsPlus">1588.00</span></td>
+                    <td><a href="javascript:;">查看</a></td>
+                </tr>
+                <tr>
+                    <td ><a href="#" title="" class="webStatsLink">980</a></td>
+                    <td>VIVO</td>
+                    <td>MP551538</td>
+                    <td><span class="statsMinus">1588.00</span></td>
+                    <td><a href="javascript:;">查看</a></td>
+                </tr>
+                <tr>
+                    <td><a href="#" title="" class="webStatsLink">980</a></td>
+                    <td>金立</td>
+                    <td>MP551538</td>
+                    <td><span class="statsPlus">1588.00</span></td>
+                    <td><a href="javascript:;">查看</a></td>
+                </tr>
+                <tr>
+                    <td><a href="#" title="" class="webStatsLink">980</a></td>
+                    <td>华为</td>
+                    <td>MP551538</td>
+                    <td><span class="statsMinus">1588.00</span></td>
+                    <td><a href="javascript:;">查看</a></td>
+                </tr>
+                <tr>
+                    <td><a href="#" title="" class="webStatsLink">980</a></td>
+                    <td>OPPO</td>
+                    <td>MP551538</td>
+                    <td><span class="statsPlus">1588.00</span></td>
+                    <td><a href="javascript:;">查看</a></td>
+                </tr>
+                <tr>
+                    <td><a href="#" title="" class="webStatsLink">980</a></td>
+                    <td>OPPO</td>
+                    <td>MP551538</td>
+                    <td><span class="statsMinus">1588.00</span></td>
+                    <td><a href="javascript:;">查看</a></td>
+                </tr>
+                <tr>
+                    <td><a href="#" title="" class="webStatsLink">980</a></td>
+                    <td>vivo</td>
+                    <td>MP551538</td>
+                    <td><span class="statsPlus">1588.00</span></td>
+                    <td><a href="javascript:;">查看</a></td>
+                </tr>
+                <tr>
+                    <td><a href="#" title="" class="webStatsLink">980</a></td>
+                    <td>returned visitors</td>
+                    <td>MP551538</td>
+                    <td><span class="statsMinus">1588.00</span></td>
+                    <td><a href="javascript:;">查看</a></td>
+                </tr>
+                <tr>
+                    <td><a href="#" title="" class="webStatsLink">980</a></td>
+                    <td>returned visitors</td>
+                    <td>MP551538</td>
+                    <td><span class="statsPlus">1588.00</span></td>
+                    <td><a href="javascript:;">查看</a></td>
+                </tr>
+                <tr>
+                    <td><a href="#" title="" class="webStatsLink">980</a></td>
+                    <td>returned visitors</td>
+                    <td>MP551538</td>
+                    <td><span class="statsMinus">1588.00</span></td>
+                    <td><a href="javascript:;">查看</a></td>
+                </tr>
+            </tbody>
+        </table>    
+    </div> 
+    <div class="clear"></div>
+</div>
+
---
+++ runtime/temp/a4d1f730c2f6b0b995e3a6316e6fccd0.php
@@ -0,0 +1,23 @@
+<?php if (!defined('THINK_PATH')) exit(); /*a:1:{s:79:"D:\data\wwwroot\yitian\admin\public/../application/index\view\index\topnav.html";i:1525851207;}*/ ?>
+<div class="topNav">
+    <div class="wrapper">
+        <div class="welcome"><a href="#" title=""><img src="static/images/userPic.png" alt="" /></a><span>welcome to here</span></div>
+        <div class="userNav">
+            <ul>
+                <li><a href="#" title=""><img src="static/images/icons/topnav/profile.png" alt="" /><span>个人中心</span></a></li>
+                <li><a href="#" title=""><img src="static/images/icons/topnav/tasks.png" alt="" /><span>任务</span></a></li>
+                <li class="dd"><a title=""><img src="static/images/icons/topnav/messages.png" alt="" /><span>消息</span><span class="numberTop">8</span></a>
+                    <ul class="userDropdown">
+                        <li><a href="#" title="" class="sAdd">new message</a></li>
+                        <li><a href="#" title="" class="sInbox">inbox</a></li>
+                        <li><a href="#" title="" class="sOutbox">outbox</a></li>
+                        <li><a href="#" title="" class="sTrash">trash</a></li>
+                    </ul>
+                </li>
+                <li><a href="#" title=""><img src="static/images/icons/topnav/settings.png" alt="" /><span>设置</span></a></li>
+                <li><a href="#" title=""><img src="static/images/icons/topnav/logout.png" alt="" /><span>退出</span></a></li>
+            </ul>
+        </div>
+        <div class="clear"></div>
+    </div>
+</div>
